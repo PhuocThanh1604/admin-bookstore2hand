eb93d8e07ee2652f9bc68e46be2eff7c9ab6fc43
thanhvp <phuocthanh20cgh@gmail.com>
2023-07-18 21:53:56 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import store from './store'; // Import your Redux store
+import { Provider } from 'react-redux';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+);
